2ff18f704851d2cee2edb045e1665462db65ea33
coltondane <113216904+coltondane@users.noreply.github.com>
2023-09-10 01:43:50 -0600

    reconstructing files to get rid of svg.js

--- Main/index.js
+++ Main/index.js
@@ -1,7 +1,40 @@
-// import libraires/modules
-const Logo = require('./lib/svg.js');
+// import libraries
+const inquirer = require('inquirer');
+const{ log } = require('console');
+const Circle = require('./lib/shapes.js');
+const Square = require('./lib/shapes.js');
+const Triangle = require('./lib/shapes.js');
 
-const logo = new Logo();
+// prompt the user
+function prompts() {
+    return inquirer
+        .prompt([
+             {
+                type: 'input',
+                name: 'text',
+                message: 'Enter up to three characters for your logo',
+            },
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'Enter the color for your text (Hexidecimal accepted)',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose what shape you would like your logo to use', 
+                choices: ['Triangle', 'Circle', 'Square'],
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'Enter the color for your shape (Hexidecimal accepted)',
+            }
+        ])
+        // Create the logo
+        .then((answers) => {
+            log(answers);
+        })
+    }
 
-logo.prompts();
-
+prompts();
--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -1,17 +1,39 @@
 class Circle {
     constructor(text, textColor, shape, shapeColor) {
+        this.text = text;
+        this.textColor = textColor;
+        this.shape = shape;
+        this.shapeColor = shapeColor;
+    }
+    makeCircle() {
         this.shape = '<svg width="250" height="250"> <circle cx="125" cy="125" r="100" fill="#aeaeae" /> <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="100px" font-family="Arial" dy=".3em">BC</text> Sorry, your browser does not support inline SVG. </svg>'
     }
 }
 
 class Triangle {
-    constructor(text, textColor, shape, shapeColore) {
+    constructor(text, textColor, shape, shapeColor) {
+        this.text = text;
+        this.textColor = textColor;
+        this.shape = shape;
+        this.shapeColor = shapeColor;
         this.shape = '<svg height="250" width="250"><polygon points="225,10 100, 210 350, 210" style="fill:lime" /> <text x="150" y="125" text-anchor="middle" fill="white" font-size="100px" font-family="Arial" dy=".3em">BC</text> Sorry, your browser does not support inline SVG. </svg>'
+    }
+    makeTriangle() {
+        this.shape = '<svg width="250" height="250"> <circle cx="125" cy="125" r="100" fill="#aeaeae" /> <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="100px" font-family="Arial" dy=".3em">BC</text> Sorry, your browser does not support inline SVG. </svg>'
     }
 }
 
 class Square {
     constructor(text, textColor, shape, shapeColor) {
+        this.text = text;
+        this.textColor = textColor;
+        this.shape = shape;
+        this.shapeColor = shapeColor;
         this.shape = '<svg xmlns="http://www.w3.org/2000/svg"><rect x="0" y="0" width="100" height="100" fill="red"></rect><text x="0" y="50" font-family="Verdana" font-size="35fill="blue">Hello</text></svg>'
     }
+    makeSquare() {
+        this.shape = '<svg width="250" height="250"> <circle cx="125" cy="125" r="100" fill="#aeaeae" /> <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="100px" font-family="Arial" dy=".3em">BC</text> Sorry, your browser does not support inline SVG. </svg>'
+    }
 }
+
+module.exports = Circle, Square, Triangle;
